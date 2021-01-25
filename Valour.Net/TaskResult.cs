cdc081abcfce83f9d82692107d3c249b2ef7f250
Niami <36048277+NiamiTV@users.noreply.github.com>
2021-01-25 20:19:10 +0000

    restructure attempt

---
+++ Valour.Net/TaskResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Valour.Net
+{
+    public class TaskResult<T>
+    { 
+        public string Info { get; set; }
+        public bool Success { get; set; }
+        public T Data { get; set; }
+
+        public TaskResult(bool success, string info, T data)
+        {
+            Success = success;
+            Info = info;
+            Data = data;
+        }
+        
+        public TaskResult(string info)
+        {
+            Success = false;
+            Info = info;
+        }
+
+        public override string ToString()
+        {
+            return Success ? $"[SUCCESS] {Info}" : $"[FATAL] {Info}";
+        }
+    }
+}
