b398da57e71c51e45483779f279d8c72d0ee3818
Niami <niamitv@gmail.com>
2021-08-08 22:14:57 +0100

    ValourUser and PlanetMember converters now accept pings

--- Valour.Net/TypeConverters/ValourUserConverter.cs
+++ Valour.Net/TypeConverters/ValourUserConverter.cs
@@ -5,6 +5,7 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Valour.Net.CommandHandling;
 using Valour.Net.Models;
 
 namespace Valour.Net.TypeConverters
@@ -18,13 +19,29 @@ namespace Valour.Net.TypeConverters
 
         public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
         {
+            CommandContext ctx = (context as CommandArgConverterContext).ctx;
+
             object result = null;
             string stringValue = (string)value;
             if (!string.IsNullOrEmpty(stringValue))
             {
+                
                 if (ulong.TryParse(stringValue, out ulong UserID)) // Input as id
                 {
                     result = Cache.GetValourUser(UserID).Result;
+                }
+                else if (stringValue.Substring(0, 4) == "«@m-") //input as ping 
+                {
+                    if (ulong.TryParse(stringValue.Substring(4, 15), out ulong MemberID))
+                    {
+                        
+                        result = Cache.GetValourUser(Cache.PlanetMemberCache.Values.FirstOrDefault(x => x.Planet_Id == ctx.Planet.Id && x.Id == MemberID).User_Id).Result;
+                    }
+                    else
+                    {
+                        result = null;
+                    }
+
                 }
                 else //Input as name
                 {
